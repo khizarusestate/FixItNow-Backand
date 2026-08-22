6b0c70a4a9ad8b0b8e34f74613136b7f6377075f
khizarusestate <khizarusestate@gmail.com>
2026-08-22 15:06:52 +0500

    Fix HttpOnly auth responses to avoid exposing tokens

--- utils/attachAuthResponse.js
+++ utils/attachAuthResponse.js
@@ -1,8 +1,17 @@
-import { setAuthCookies } from "./authCookies.js";
+import { setAuthCookies, USE_HTTPONLY_AUTH } from "./authCookies.js";
 
-/** Attach tokens to JSON body (legacy) and httpOnly cookies when enabled. */
+/**
+ * Attach auth credentials to the response.
+ * In HttpOnly-cookie mode, tokens are delivered only through cookies and are
+ * never exposed in the JSON response body.
+ */
 export function attachAuthToResponse(res, { accessToken, refreshToken, body }) {
   setAuthCookies(res, { accessToken, refreshToken });
+
+  if (USE_HTTPONLY_AUTH) {
+    return { ...body };
+  }
+
   return {
     ...body,
     token: accessToken,
